f5fe55d600252d521741439583131d7b40f953d3
Andersson Camilo Cárdenas Guarin <86067848+anderssonccg@users.noreply.github.com>
2025-05-08 17:37:54 -0500

    refactor: move models to app folder

---
+++ app/models.py
@@ -0,0 +1,12 @@
+from typing import Annotated, Optional
+from sqlmodel import Field, SQLModel
+
+class NoteBase(SQLModel):
+    title: str
+    content: str
+
+class Note(NoteBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+class NoteCreate(NoteBase):
+    pass
